e3c14fcad27df0d80327a170c39b272ef43e2ba5
Rintaro Okamura <rintaro.okamura@gmail.com>
2015-11-18 22:45:51 +0900

    Add lut_plot.rb

--- README.md
+++ README.md
@@ -34,7 +34,7 @@ Look-up table used in this program should be like following:
 
 ex.)
 
-| COT | CDER | R1   | R2   |
+| COT | CDER | REF1 | REF2 |
 |:----|:-----|:-----|:-----|
 | 1.0 | 3.0  | 0.20 | 0.10 |
 | 1.0 | 5.0  | 0.20 | 0.13 |
@@ -53,8 +53,17 @@ ex.)
 This file should be given as a binary format.
 
 
+Utils
+---
+
+* lut\_plot.rb
+
+make a Nakajima-King-like plot of the look-up table.
+
+
 References
 ---
 
 * [Teruyuki Nakajima and Michael D. King, 1990: Determination of the Optical Thickness and Effective Particle Radius of Clouds from Reflected Solar Radiation Measurements. Part I: Theory. J. Atmos. Sci., 47, 1878–1893.](http://journals.ametsoc.org/doi/abs/10.1175/1520-0469(1990)047%3C1878%3ADOTOTA%3E2.0.CO%3B2)
+* [Hiroshi Akima, 1970: A new method of interpolation and smooth curve fitting based on local procedures. JACM, 17.4, 589–602.](http://dl.acm.org/citation.cfm?id=321609)
 
---
+++ utils/plot/lut_plot.rb
@@ -0,0 +1,111 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+#
+# lut_plot.rb
+#
+# Author: Rintaro Okamura
+#
+# Description:
+#   This script makes a plot of look-up table.
+#
+# Dependency:
+#   NArray gem
+#   gnuplot gem
+#
+# Changelog:
+#   20151118: First version
+#
+
+require 'narray'
+require 'gnuplot'
+
+
+if ARGV.size < 3 then
+  STDERR.puts <<heredoc
+Usage: ruby #{$0} input output numrecord
+  input:  lut filename
+  output: output png filename
+  numrecord = (# of tau) * (# of cder)
+heredoc
+  exit
+end
+
+inpfilepath = ARGV[0]
+outfilepath = ARGV[1]
+numrecord   = ARGV[2].to_i
+
+if File.exist?(File.expand_path(inpfilepath)) then
+  datafile = open(File.expand_path(inpfilepath), 'r+b')
+  lut = NArray.sfloat(4, numrecord)
+  tmpstr = datafile.read(4*4*numrecord)
+  lut[true,true] = NArray.to_na(tmpstr, "sfloat", 4, numrecord)
+else
+  puts "There's no file."
+  exit
+end
+
+unqtaus  = lut.transpose(1,0).to_a[0].uniq
+unqcders = lut.transpose(1,0).to_a[1].uniq
+
+Gnuplot.open do |gp|
+  Gnuplot::Plot.new(gp) do |plot|
+    plot.terminal "png"
+    plot.output File.expand_path("lut.png")
+    # plot.terminal 'x11'
+
+    plot.title "look-up table"
+    plot.xlabel "Reflectance 1"
+    plot.ylabel "Reflectance 2"
+
+    plot.xrange "[0.0:1.2]"
+    plot.yrange "[0.0:0.6]"
+
+    plot.set "key off"
+
+    clr = 30
+
+    unqcders.each do |cder|
+      ls = lut.to_a.select do |l|
+        l[1] == cder
+      end
+
+      pltref1 = []
+      pltref2 = []
+
+      ls.each do |l|
+        pltref1.push(l[2])
+        pltref2.push(l[3])
+      end
+
+      plot.data << Gnuplot::DataSet.new([pltref1, pltref2]) do |ds|
+        ds.with = "lines"
+        ds.linewidth = 2
+        ds.linecolor = "rgb \"##{format('%02x', clr)}33#{format('%02x', clr)}\""
+      end
+      clr += 225 / unqcders.length
+    end
+    unqtaus.each do |tau|
+      ls = lut.to_a.select do |l|
+        l[0] == tau
+      end
+
+      pltref1 = []
+      pltref2 = []
+
+      ls.each do |l|
+        pltref1.push(l[2])
+        pltref2.push(l[3])
+      end
+
+      plot.data << Gnuplot::DataSet.new([pltref1, pltref2]) do |ds|
+        ds.with = "lines"
+        # ds.linestyle = 2
+        ds.linecolor = "black"
+      end
+    end
+
+  end
+end
+
+
